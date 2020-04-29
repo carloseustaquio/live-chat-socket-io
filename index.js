183a2436228482c5477c050fb0b9207edee46e33
Carlos Eustáquio <caeu227@gmail.com>
2020-04-29 09:02:02 -0300

    adjusts server and index.html

--- index.js
+++ index.js
@@ -1,25 +1,57 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const express = require('express');
+const http = require('http')
+const path = require('path')
+const socketIO = require('socket.io')
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html')
+})
 
 io.on('connection', socket => {
-  console.log('a user connected!');
+  // console.log("new user")
 
-  socket.on('chat message', msg => {
-    console.log('message: ', msg)
+  socket.username = "Anonymus"
 
-    io.emit('chat message', msg)
+  socket.on('change_username', data => {
+    socket.username = data.username
+    // console.log('username: ', socket.username)
   })
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected')
+  socket.on('chat_message', data => {
+    console.log('data: ', data, socket.username)
+    io.emit('chat_message', { msg: data, user: socket.username })
   })
 })
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+server.listen(3000, () => console.log("listening on port 3000"))
+
+// const app = require('express')();
+// const http = require('http').createServer(app);
+// const io = require('socket.io')(http);
+
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// io.on('connection', socket => {
+//   console.log('a user connected!');
+
+//   socket.on('chat_message', msg => {
+//     console.log('message: ', msg)
+
+//     io.emit('chat_message', msg)
+//   })
+
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected')
+//   })
+// })
+
+// http.listen(3000, function(){
+//   console.log('listening on *:3000');
+// });
